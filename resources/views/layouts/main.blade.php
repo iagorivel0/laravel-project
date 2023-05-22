d404f93df3cb7a4d38b97f8a3418de90773eee62
Iago Augusto <iago.rivelo@gmail.com>
2023-05-22 02:08:59 -0300

    Created Layouts, Model and Controller

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,20 +6,50 @@
 
         <title>@yield('title')</title>
 
+        <link rel="shortcut icon" href="./img/hdcevents_logo.svg" type="image/x-icon">
+
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+
+        <!-- Styles Boostrap-->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="./css/styles.css">
+        <link rel="stylesheet" href="/css/styles.css">
 
         <!-- Scripts -->
-        <script src="./js/scripts.js"></script>
+        <script src="/js/scripts.js"></script>
     </head>
     <body class="antialiased">
+      <header>
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <div class="collapse navbar-collapse" id="navbar">
+            <a href="/" class="navbar-brand">
+              <img src="/img/hdcevents_logo.svg" alt="HDC Events">
+            </a>
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a href="/" class="nav-link">Eventos</a>
+              </li>
+              <li class="nav-item">
+                <a href="/events/create" class="nav-link">Criar Eventos</a>
+              </li>
+              <li class="nav-item">
+                <a href="/" class="nav-link">Entrar</a>
+              </li>
+              <li class="nav-item">
+                <a href="/" class="nav-link">Cadastrar</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
       @yield('content')
       <footer>
         <p>HDC Events &copy; 2023</p>
       </footer>
+      <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
     </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,10 @@
 
 @section('title', 'HDC Events')
 
+  @foreach ($events as $event)
+      <p>{{ $event->title }} -- {{ $event->description }}</p>
+  @endforeach
+
 @section('content')
 
 @endsection
