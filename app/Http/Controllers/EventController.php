cc59d0cfc93bc2a9f513150df726b867d9c0f4a0
Iago Augusto <iago.rivelo@gmail.com>
2023-05-29 00:23:37 -0300

    Adding the code for quick search to work

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -9,10 +9,22 @@ class EventController extends Controller
 {
     public function index()
     {
-        $events = Event::all();
+        $search = request('search');
+
+        if($search)
+        {
+            $events = Event::where([
+                ['title', 'like', '%'.$search.'%']
+            ])->get();
+        }
+        else
+        {
+            $events = Event::all();
+        }
 
         return view('welcome', [
-            'events' => $events
+            'events' => $events,
+            'search' => $search
         ]);
     }
     
@@ -26,7 +38,7 @@ class EventController extends Controller
         $event = new Event;
 
         $event->title       = $request->title;
-        $event->date       = $request->date;
+        $event->date        = $request->date;
         $event->city        = $request->city;
         $event->private     = $request->private;
         $event->description = $request->description;
